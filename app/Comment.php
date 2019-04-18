5a783b0514b939f128a102d7f0491925dc5d4f54
Tehnikumi Opilane <opilane@tptlive.ee>
2019-04-18 13:53:38 +0300

    Add comment liking functionality

--- app/Comment.php
+++ app/Comment.php
@@ -16,4 +16,7 @@ class Comment extends Model
     public function post(){
         return $this->belongsTo(Post::class);
     }
+    public function likes(){
+        return $this->hasMany(CommentLike::class);
+    }
 }
